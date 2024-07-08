398d402574aa8b6ebd414f68a8791476ce25b3f4
JSScot <153024062+JSScot@users.noreply.github.com>
2024-07-08 22:02:10 +0000

    colors

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 /* VARIABLES */
+let eyeHeight = 30
+let eyeWidth = 55
 
+let pupHeight =15
+let pupWidth =27
 /* SETUP RUNS ONCE */
 function setup() {
   //sets the screen size
   createCanvas(400,400); 
 
   //sets the background color
-  background(255,249,226); 
+  background("#e8e1ba"); 
 }
 
 /* DRAW LOOP REPEATS */
@@ -15,20 +19,26 @@ function draw() {
   rectMode(CENTER)
 
   //Face
-  ellipse(200,200,175,200)
+  fill("#914e24")
+  ellipse(width/2,height/2,175,200)
 
   //eyes
-  ellipse(155,180,55,30)
-  ellipse(245,180,55,30)
+  fill("white")
+  ellipse(155,180,eyeWidth,eyeHeight)
+  ellipse(245,180,eyeWidth,eyeHeight)
 
   //pupils
-  ellipse(155,180,27,15)
-  ellipse(245,180,27,15)
+  fill("#3b1d0b")
+  ellipse(155,180,pupWidth,pupHeight)
+  ellipse(245,180,pupWidth,pupHeight)
 
   //mouth
+  fill("#914e24")
+  strokeWeight(3)
   arc(200,230,30,50,0,180)
 
   //text
+  fill("red")
   textSize(17)
   text("We will customize this text later on\n in the project to reflect your avatar",10,20)
 }
