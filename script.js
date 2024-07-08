f5511fe4256a574db7d91b8212cb33db4ea2df8c
JSScot <153024062+JSScot@users.noreply.github.com>
2024-07-08 21:25:51 +0000

    base of face is done

--- script.js
+++ script.js
@@ -11,5 +11,24 @@ function setup() {
 
 /* DRAW LOOP REPEATS */
 function draw() {
-  
+  angleMode(DEGREES)
+  rectMode(CENTER)
+
+  //Face
+  ellipse(200,200,175,200)
+
+  //eyes
+  ellipse(155,180,55,30)
+  ellipse(245,180,55,30)
+
+  //pupils
+  ellipse(155,180,27,15)
+  ellipse(245,180,27,15)
+
+  //mouth
+  arc(200,230,30,50,0,180)
+
+  //text
+  textSize(17)
+  text("We will customize this text later on\n in the project to reflect your avatar",10,20)
 }
