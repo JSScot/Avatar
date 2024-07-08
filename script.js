a337e781922e9b9d709d4ca26b93232088a73948
JSScot <153024062+JSScot@users.noreply.github.com>
2024-07-08 20:21:18 +0000

    change

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 /* SETUP RUNS ONCE */
 function setup() {
   //sets the screen size
-  createCanvas(700,700); 
+  createCanvas(400,400); 
 
   //sets the background color
   background(255,249,226); 
