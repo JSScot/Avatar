75bef07ca9ecdb3197318b4c94ffea79e86d96b4
JSScot <153024062+JSScot@users.noreply.github.com>
2024-07-09 15:30:37 +0000

    Added nose and earrings

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 /* VARIABLES */
+let toggle = false
+
 let eyeHeight = 30
 let eyeWidth = 55
 
@@ -18,40 +20,87 @@ function draw() {
   angleMode(DEGREES)
   rectMode(CENTER)
 
-  /*hair
+  //hair
+  stroke("black")
   fill("black")
-  rect(width/2,260,200,160)
-  ellipse(136,118,100,200)*/
+  ellipse(200,170,235,200)
+  ellipse(200,170,235,200)
+  ellipse(79,224,130,130)
+  ellipse(320,224,130,130)
+
 
   //ears
-  fill("#914e24")
+  fill("#854a25")
   ellipse(106,192,30,50)
   ellipse(294,192,30,50)
 
+  //earrings
+  fill("#c9c8c7")
+  rect(106,229,10,40)
+  rect(295,229,10,40)
+
   
   //Face
-  fill("#914e24")
+  fill("#854a25")
   ellipse(width/2,height/2,175,200)
   
 
   //eyes
-  fill("white")
-  ellipse(155,180,eyeWidth,eyeHeight)
-  ellipse(245,180,eyeWidth,eyeHeight)
+  //toggleEyes
+  if(toggle==true)
+  {
+    fill(0)
+    ellipse(155,180,eyeWidth,eyeHeight/8)
+    ellipse(245,180,eyeWidth,eyeHeight/8)
+    //pupils
+    fill("#3b1d0b")
+    ellipse(155,180,pupWidth,pupHeight/8)
+    ellipse(245,180,pupWidth,pupHeight/8)
+  }
+  else if(toggle==false){
+    fill("white")
+    ellipse(155,180,eyeWidth,eyeHeight)
+    ellipse(245,180,eyeWidth,eyeHeight)
+    //pupils
+    fill("#3b1d0b")
+    ellipse(155,180,pupWidth,pupHeight)
+    ellipse(245,180,pupWidth,pupHeight)
+  }
 
-  //pupils
-  fill("#3b1d0b")
-  ellipse(155,180,pupWidth,pupHeight)
-  ellipse(245,180,pupWidth,pupHeight)
+  //nose
+  fill("#854a25")
+  arc(width/2,215,30,20,0,180)
+
 
   //mouth
-  fill("#914e24")
-  strokeWeight(3)
-  arc(200,230,30,50,0,180)
+  if(toggle==true)
+    {
+      //close
+      fill("#854a25")
+      arc(200,245,30,50,0,180)
+    }
+  else
+  {
+    //open mouth
+    fill("#e88372")
+    strokeWeight(3)
+    arc(200,245,30,50,0,180)
+  }
 
 
   //text
-  fill("red")
-  textSize(17)
-  text("We will customize this text later on\n in the project to reflect your avatar",10,20)
+  fill("#079ade")
+  stroke("white")
+  textSize(20)
+  text("Hi, I'm Jordin. I like coding, crocheting \n and baking stuff",10,20)
+
+  //Directions for Mouse Press
+   textSize(17)
+  text("Click to see me close my eyes.\nClick again to open them",150,370)
+}
+
+/* Functions */
+function mousePressed()
+{
+  toggle = !toggle
 }
